089231b778fb9a03a30049a8e3eea57af504bd5f
Aziz Boudi <azicode4@gmail.com>
2016-10-12 11:25:27 -0700

    Added Child.js and Sibling.js from CodeCademy lesson on passing props

--- App.js
+++ App.js
@@ -1,58 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Child = require('./Child');
+var Sibling = require('./Sibling');
 
-class App extends React.Component {
-	constructor(){
-		super();
-		this.state = { 
-			red: 0,
-			green: 0,
-			blue: 0
-		}
-		this.update = this.update.bind(this)
-	}
+var App = React.createClass({
+  getInitialState: function () {
+    return { name: 'Frarthur' };
+  },
+  
+  changeName: function (newName) {
+    this.setState({
+      name: newName
+    });
+  },
 
-	update(e){
-		this.setState({
-			red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-			green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-			blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
-		})
-	}
-
-	render(){
-
-		return (
-			<div >
-				<Slider ref='red'  update={this.update} />
-				{this.state.red}
-				<br />
-				<Slider ref='green' update={this.update} />
-				{this.state.green}
-				<br />
-				<Slider ref="blue" update={this.update} />
-				{this.state.blue}
-				<br />
-			</div>
-			);
-	}
-}
-
-class Slider extends React.Component {
-	render(){
-		return(
-		  <div>	
-			<input ref="inp" type="range"
-			  min="0"
-			  max="255"
-			  onChange={this.props.update} />
-		  </div>	  
-			);
-	}
-}
-
-
-
-
+  render: function () {
+    return (
+      <div>
+        <Child 
+          onChange={this.changeName} />
+        <Sibling name={this.state.name}/>
+      </div>
+    );
+  }
+});
 
 export default App
